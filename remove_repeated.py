83eb116691a05ce5ec70450cd0d81d22844231b0
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2014-12-21 19:32:44 -0500

    added three things

---
+++ remove_repeated.py
@@ -0,0 +1,82 @@
+import os,sys,codecs
+
+def has_rep(train_sens,dev_sens,test_sens,train_writer,dev_writer,test_writer):
+	print 'checking for repeated sentences'
+	sen_dic=dict()
+	rep=0
+	cnt=0
+	for sen in train_sens:
+		cnt+=1
+		sen=sen.strip()
+		if not sen:
+			continue
+		ls=sen.split('\n')
+		ws=list()
+		for l in ls:
+			ws.append(l.split('\t')[1].strip())
+
+		sentence=' '.join(ws)
+		if sen_dic.has_key(sentence):
+			print 'train'
+			rep+=1
+			#return True
+		else:
+			sen_dic[sentence]=sen
+			train_writer.write(sen+'\n\n')
+
+	for sen in dev_sens:
+		cnt+=1
+		sen=sen.strip()
+		if not sen:
+			continue
+		ls=sen.split('\n')
+		ws=list()
+		for l in ls:
+			ws.append(l.split('\t')[1].strip())
+
+		sentence=' '.join(ws)
+		if sen_dic.has_key(sentence):
+			print 'dev'
+			rep+=1
+			#return True
+		else:
+			sen_dic[sentence]=sen
+			dev_writer.write(sen+'\n\n')
+
+	for sen in test_sens:
+		cnt+=1
+		sen=sen.strip()
+		if not sen:
+			continue
+		ls=sen.split('\n')
+		ws=list()
+		for l in ls:
+			ws.append(l.split('\t')[1].strip())
+
+		sentence=' '.join(ws)
+		if sen_dic.has_key(sentence):
+			print 'test'
+			rep+=1
+			#return True
+		else:
+			sen_dic[sentence]=sen
+			test_writer.write(sen+'\n\n')
+	train_writer.flush()
+	train_writer.close()
+	dev_writer.flush()
+	dev_writer.close()
+	test_writer.flush()
+	test_writer.close()
+	print rep
+
+
+train_sens=codecs.open(os.path.abspath(sys.argv[1]),'r',encoding='utf-8').read().split('\n\n')
+dev_sens=codecs.open(os.path.abspath(sys.argv[2]),'r',encoding='utf-8').read().split('\n\n')
+test_sens=codecs.open(os.path.abspath(sys.argv[3]),'r',encoding='utf-8').read().split('\n\n')
+print len(train_sens),len(dev_sens),len(test_sens)
+
+train_writer=codecs.open(os.path.abspath(sys.argv[1])+'.ref','w',encoding='utf-8')
+dev_writer=codecs.open(os.path.abspath(sys.argv[2])+'.ref','w',encoding='utf-8')
+test_writer=codecs.open(os.path.abspath(sys.argv[3])+'.ref','w',encoding='utf-8')
+
+has_rep(train_sens,dev_sens,test_sens,train_writer,dev_writer,test_writer)
---
+++ sanity_check_data.py
@@ -0,0 +1,58 @@
+import os,sys,codecs
+
+def trav(rev_head,h,visited):
+	if rev_head.has_key(h):
+		for d in rev_head[h]:
+			if d in visited:
+				return True
+			visited.append(d)
+			trav(rev_head,d,visited)
+	return False
+
+def has_loop(sens):
+	print 'checking for repeated sentences'
+	sen_dic=dict()
+	rep=0
+	cnt=0
+	
+	for sen in sens:
+		cnt+=1
+		if cnt%1000==0:
+			sys.stdout.write(str(cnt)+'...')
+		sen=sen.strip().replace('\r','')
+		if not sen:
+			continue
+		ls=sen.split('\n')
+		hd=dict()
+		rev_head=dict()
+		i=1
+		for l in ls:
+			if len(l.split('\t'))<7:
+				print len(l.split('\t'))
+				print l
+				print l.split('\t')
+				continue
+			h=int(l.split('\t')[6].strip())
+			hd[i]=int(h)
+			if not rev_head.has_key(h):
+				rev_head[h]=list()
+			rev_head[h].append(i)
+			i+=1
+
+		visited=list()
+		#print rev_head
+		if trav(rev_head,0,visited):
+			print sen
+		#if len(visited)<len(ls):
+			#print sen
+
+
+	sys.stdout.write('\n')
+
+
+
+sens=codecs.open(os.path.abspath(sys.argv[1]),'r',encoding='utf-8').read().split('\n\n')
+print len(sens)
+#sen_writer=codecs.open(os.path.abspath(sys.argv[1])+'.ref','w',encoding='utf-8')
+
+has_loop(sens)
